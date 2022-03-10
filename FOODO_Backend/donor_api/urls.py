60342f7e1d1ab7ad7eb5b92dc68ad305fc28c51c
dikshita18 <60962844+dikshita18@users.noreply.github.com>
2022-03-10 11:52:37 +0530

    Update urls.py

--- FOODO_Backend/donor_api/urls.py
+++ FOODO_Backend/donor_api/urls.py
@@ -17,22 +17,3 @@ urlpatterns=[
 
 
 
-
-
-
-'''
-
-from django.urls import path
-from django.urls import path,include
-from rest_framework import routers
-from .views import UserViewSet, DonateFormViewSet, FeedbackFormViewSet, DonateProcessViewSet
-router=routers.DefaultRouter()
-router.register(r'user',UserViewSet)
-router.register(r'donateform',DonateFormViewSet)
-router.register(r'feedbackform',FeedbackFormViewSet)
-router.register(r'donateprocess',DonateProcessViewSet)
-urlpatterns=[
-    path('',include(router.urls))
-]
-
-'''
