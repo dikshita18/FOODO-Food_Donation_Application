030d08f25a7ac6fa743dc7838a0267e6b773270e
dikshita18 <60962844+dikshita18@users.noreply.github.com>
2022-03-10 11:52:08 +0530

    Update serializers.py

--- FOODO_Backend/donor_api/serializers.py
+++ FOODO_Backend/donor_api/serializers.py
@@ -27,32 +27,3 @@ class DonateProcessSerializer(serializers.ModelSerializer):
 
 
 
-
-
-'''
-from rest_framework import serializers
-from .models import User, DonateForm, FeedbackForm, DonateProcess
-from rest_framework.authtoken.views import Token
-
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ('id','full_name','email_id','ngo_name','address','contact','pwd')
-
-class DonateFormSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = DonateForm
-        fields = ('id','full_name','cont_no','food_type','cdt','pdt','ploc')
-        
-class FeedbackFormSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = FeedbackForm
-        fields = ('id','full_name','email_id','cont_no','feedback')
-
-class DonateProcessSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = DonateProcess
-        fields = ('id','don_id')
-
-
-'''
